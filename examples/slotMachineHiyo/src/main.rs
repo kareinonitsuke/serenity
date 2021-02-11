3253c0c671f9514b655e14f1c0652f8630a0f1c1
kmsmmk0311@gmail.com <kmsmmk0311@gmail.com>
2021-02-11 20:13:33 +0900

    [clean] clean main.rc
    main.rc　rustの仕様がわからず、汚くなっていたところを修正

--- examples/slotMachineHiyo/src/main.rs
+++ examples/slotMachineHiyo/src/main.rs
@@ -13,8 +13,8 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        let momo = ":baby_chick:";
-        let momogang = ":hatching_chick:";
+        let momo = "<:momo:747707481282838588>";
+        let momogang = "<:momogang:747708446878728233>";
         let picture_num:i16 = 2;
         let slot_num:i16 = 3;
 
@@ -28,38 +28,27 @@ impl EventHandler for Handler {
                 },
             };
 
-            let mut picture = ["a","b","c"];
-            for _n in 1..slot_num+1{
+            let mut pictures = ["","",""];
+            for a_picture in pictures.iter_mut() {
                 let rand_num :i16 = rand::thread_rng().gen_range(0, picture_num);
-                let i:i16 = _n;
-                match i{//配列の指定のところ1だとうまくいかないので暫定　そもそもどう書くべきだろうか
-                    1=> match rand_num{
-                        0=> picture[0] = momo,
-                        1=> picture[0] = momogang,
-                        _=> picture[0] = "error"
-                        },
-                    2=> match rand_num{
-                        0=> picture[1] = momo,
-                        1=> picture[1] = momogang,
-                        _=> picture[1] = "error"
-                        },
-                    3=> match rand_num{
-                        0=> picture[2] = momo,
-                        1=> picture[2] = momogang,
-                        _=> picture[2] = "error"
-                        },
-                    _ => println!("Error"),
+                match rand_num{
+                    0=> *a_picture = momo,
+                    1=> *a_picture = momogang,
+                    _=> *a_picture = "error"
                 }
-                if _n == 3{//forの外で結果とるのどうしたらいいかわからんかったので暫定
-                    let response = MessageBuilder::new()
-                    //.mention(&msg.author)
-                    //.push(" ")
-                    .push(picture[0].to_string()+" "+picture[1]+" "+picture[2])
-                    .build();
-                    if let Err(why) = msg.channel_id.say(&ctx.http, &response).await {
-                        println!("Error sending message: {:?}", why);
-                    }
-                }
+            }
+            
+            let response = MessageBuilder::new()
+            //.mention(&msg.author)
+            //.push(" ")
+            .push(pictures[0].to_string())
+            .push(" ")
+            .push(pictures[1].to_string())
+            .push(" ")
+            .push(pictures[2].to_string())
+            .build();
+            if let Err(why) = msg.channel_id.say(&ctx.http, &response).await {
+                println!("Error sending message: {:?}", why);
             }
         }
     }
@@ -78,12 +67,7 @@ async fn main() {
         .event_handler(Handler)
         .await
         .expect("Err creating client");
-    // The total number of shards to use. The "current shard number" of a
-    // shard - that is, the shard it is assigned to - is indexed at 0,
-    // while the total shard count is indexed at 1.
-    //
-    // This means if you have 5 shards, your total shard count will be 5, while
-    // each shard will be assigned numbers 0 through 4.
+
     if let Err(why) = client.start_shards(2).await {
         println!("Client error: {:?}", why);
     }
